712c3c7e9bce36d73a8fbe91ac9a9bc6afdee365
3405691582 <dsk@google.com>
2025-07-29 16:48:18 +0000

    Remove allocd.
    Now that we are using cvdalloc to manage interfaces, remove the existing
    allocd implementation.

--- base/cvd/cuttlefish/host/libs/config/cuttlefish_config_instance.cpp
+++ base/cvd/cuttlefish/host/libs/config/cuttlefish_config_instance.cpp
@@ -1587,15 +1587,6 @@ void CuttlefishConfig::MutableInstanceSpecific::set_ethernet_ipv6(
   (*Dictionary())[kEthernetIPV6] = ip;
 }
 
-static constexpr char kUseAllocd[] = "use_allocd";
-bool CuttlefishConfig::InstanceSpecific::use_allocd() const {
-  return (*Dictionary())[kUseAllocd].asBool();
-}
-void CuttlefishConfig::MutableInstanceSpecific::set_use_allocd(
-    bool use_allocd) {
-  (*Dictionary())[kUseAllocd] = use_allocd;
-}
-
 static constexpr char kUseCvdalloc[] = "use_cvdalloc";
 bool CuttlefishConfig::InstanceSpecific::use_cvdalloc() const {
   return (*Dictionary())[kUseCvdalloc].asBool();
